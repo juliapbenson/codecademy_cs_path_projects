83a49a486812dde936290be3ccb6245665bd4835
Julia Benson <99056441+juliapbenson@users.noreply.github.com>
2023-05-12 09:37:02 -0700

    Add files via upload

---
+++ initials.py
@@ -0,0 +1,24 @@
+# First name = Julia
+# Last name = Benson
+# Fun fact = I'm learning to code!
+
+first_initial = "J"
+
+print(first_initial * 5)
+print("  " + first_initial + "  ")
+print("  " + first_initial + "  ")
+print("  " + first_initial + "  ")
+print(first_initial + " " + first_initial + "  ")
+print(first_initial + " " + first_initial + "  ")
+print(" " + first_initial + first_initial + "  ")
+
+print("    ")
+
+last_initial = "B"
+print(last_initial * 4 + " ")
+print(last_initial + "   " + last_initial)
+print(last_initial + "   " + last_initial)
+print(last_initial * 4 + " ")
+print(last_initial + "   " + last_initial)
+print(last_initial + "   " + last_initial)
+print(last_initial * 4 + " ")
